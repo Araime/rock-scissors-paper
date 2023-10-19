7ef396ab44251b71749fb93bb2172d4544df10eb
Nick <alvonsa09121985@gmail.com>
2023-10-19 23:22:11 +0400

    Добавьте файлы проекта.

---
+++ RSP2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include "time.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    // Первая запись обьявляет переменную rock указателем на char.
+    const char* rock = R"(
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+    Rock
+)";
+
+    const char* scissors = R"(
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+    Scissors
+)";
+
+    const char* paper = R"(
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+    Paper
+)";
+
+    cout << rock << endl << endl;
+    cout << paper << endl << endl;
+    cout << scissors << endl << endl;
+}
